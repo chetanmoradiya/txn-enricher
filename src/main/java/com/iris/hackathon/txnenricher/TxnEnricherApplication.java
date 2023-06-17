04bbe902f39d9289cd6910165428c90396b11711
chetanmoradiya <chetan.moradiya@gmail.com>
2023-06-17 19:20:57 +0530

    first commit

---
+++ src/main/java/com/iris/hackathon/txnenricher/TxnEnricherApplication.java
@@ -0,0 +1,13 @@
+package com.iris.hackathon.txnenricher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxnEnricherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxnEnricherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iris/hackathon/txnenricher/TxnEnricherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iris.hackathon.txnenricher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxnEnricherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
